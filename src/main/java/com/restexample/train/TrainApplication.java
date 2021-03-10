29c96ba497c67102934f95766a363942fbae02b5
OlehMrB <balaniukop@gmail.com>
2021-03-10 10:26:11 +0200

    Add Securing :)

--- src/main/java/com/restexample/train/TrainApplication.java
+++ src/main/java/com/restexample/train/TrainApplication.java
@@ -3,10 +3,11 @@ package com.restexample.train;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class TrainApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Throwable {
         SpringApplication.run(TrainApplication.class, args);
     }
 
